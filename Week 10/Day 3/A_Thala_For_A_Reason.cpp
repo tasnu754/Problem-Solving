704119cd3e9c4ad51876bb35d2e879fc5fb1c31e
Tasnuva Islam <tasnuvamarziya60@gmail.com>
2024-12-30 21:24:18 +0600

    Prectice

---
+++ Week 10/Day 3/A_Thala_For_A_Reason.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin>>n;
+
+    if(n==7)
+        cout<<"THALA"<<endl;
+    else
+        cout<<"SADGE"<<endl;
+
+
+    return 0;
+}
